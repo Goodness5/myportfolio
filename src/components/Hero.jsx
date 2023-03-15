35d2eee43fd6b49eeb7714d98f1a55e8f5b07c06
Goodness5 <goodnesskolapo@gmail.com>
2023-03-15 13:52:17 +0100

    fuck

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,11 +16,12 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hi, I'm <span className='text-[#915EFF]'>Goodness</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I develop websites<br className='sm:block hidden' />
+            <p>and smart contracts</p>
+            
           </p>
         </div>
       </div>
