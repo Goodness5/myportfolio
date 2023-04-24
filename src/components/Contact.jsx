d7073bb4e3fb9fabc9773cdc58e13e5f72369e18
goodness5 <goodnesskolapo@gmail.com>
2023-04-24 21:09:56 +0100

    ok

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        service_txel7ll,
+        template_v2xksna,
         {
           from_name: form.name,
           to_name: "JavaScript Mastery",
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: "goodnesskolapo@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        w4bwKHVObRANk4KFX
       )
       .then(
         () => {
